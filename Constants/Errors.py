3ed0289fdcf709491fd668753607d2ba12e8c217
Pedro Trevisan <53058763+pedro-it-Rep@users.noreply.github.com>
2021-10-19 23:33:26 -0300

    Arruma um erro encontrado no arquivo de erros

--- Constants/Errors.py
+++ Constants/Errors.py
@@ -1,71 +1,65 @@
-from Lexico import Lexico
+def exceptionCloseParenteses(line_no):
+    exit("Analisador Sintatico -> Linha : {}, missing ')' ".format((line_no + 1)))
 
 
-def exceptionCloseParenteses():
-    exit("Analisador Sintatico -> Linha : {}, missing '}' ".format((Lexico.n_line + 1)))
+def exceptionPontoVirgula(line_no):
+    exit("Analisador Sintatico -> Linha : {}, token ';' esperado.".format((line_no + 1)))
 
 
-def exceptionPontoVirgula():
-    exit("Analisador Sintatico -> Linha : {}, token ';' esperado.".format((Lexico.n_line + 1)))
+def exceptionInvalidExpression(line_no):
+    exit("Analisador Sintatico -> Linha : expressao invalida.".format((line_no + 1)))
 
 
-def exceptionInvalidExpression():
-    exit("Analisador Sintatico -> Linha : expressao invalida.".format((Lexico.n_line + 1)))
-
-
-def exceptionInvalidProcIndentifier():
+def exceptionInvalidProcIndentifier(line_no):
     exit("Analisador Sintatico -> Linha : {} nome de procedimento, identificador esperado.".format(
-        (Lexico.n_line + 1)))
+        (line_no + 1)))
 
 
-def exceptionInvalidIfDo():
-    exit("Analisador Sintatico -> Linha : {}, comando 'entao' esperado.".format((Lexico.n_line + 1)))
+def exceptionInvalidIfDo(line_no):
+    exit("Analisador Sintatico -> Linha : {}, comando se, 'entao' esperado.".format((line_no + 1)))
 
 
-def exceptionMissingDo():
-    exit("Analisador Sintatico -> Linha : {}, comando 'faca' esperado.".format((Lexico.n_line + 1)))
+def exceptionMissingDo(line_no):
+    exit("Analisador Sintatico -> Linha : {}, comando enquanto, 'faca' esperado.".format((line_no + 1)))
 
 
-def exceptionAbreParenteses():
-    exit("Analisador Sintatico -> Linha : {}, token '(' esperado.".format((Lexico.n_line + 1)))
+def exceptionAbreParenteses(line_no):
+    exit("Analisador Sintatico -> Linha : {}, token '(' esperado.".format((line_no + 1)))
 
 
-def exceptionMissingIdentifier():
-    exit("Analisador Sintatico -> Linha : {}, token 'identificador' esperado.".format((Lexico.n_line + 1)))
+def exceptionMissingIdentifier(line_no):
+    exit("Analisador Sintatico -> Linha : {}, token identificador esperado.".format((line_no + 1)))
 
 
-def exceptionTypeInvalid():
-    exit("Analisador Sintatico -> Linha : {}, tipo de variavel invalido, esperado 'boolean' ou 'inteiro'.".format((Lexico.n_line + 1)))
+def exceptionTypeInvalid(line_no):
+    exit("Analisador Sintatico -> Linha : {}, tipo de variavel invalido.".format((line_no + 1)))
 
 
-def exceptionVaribleIdentifier():
+def exceptionVaribleIdentifier(line_no):
     exit("Analisador Sintatico -> Linha : {}, nome de variavel, token identificador esperado.".format(
-        (Lexico.n_line + 1)))
+        (line_no + 1)))
 
 
-def exceptionMissingPontos():
-    exit("Analisador Sintatico -> Linha : {}, token ',' ou ':' esperado.".format((Lexico.n_line + 1)))
-
-def exceptionMissingDoisPontos():
-    exit("Analisador Sintatico -> Linha : {}, token ':' esperado.".format((Lexico.n_line + 1)))
+def exceptionMissingPontos(line_no):
+    exit("Analisador Sintatico -> Linha : {}, token ',' ou ':' esperado.".format((line_no + 1)))
 
 
-def exceptionWrongPontos():
-    exit("Analisador Sintatico -> Linha : {}, token invalido ':' apos ','.".format((Lexico.n_line + 1)))
+def exceptionWrongPontos(line_no):
+    exit("Analisador Sintatico -> Linha : {}, token invalido ':' apos ','.".format((line_no + 1)))
 
 
-def exceptionMissingPrograma():
+def exceptionMissingPrograma(line_no):
     exit("Analisador Sintatico -> Linha : {}, programa deve iniciar com o token 'programa'.".format(
-        (Lexico.n_line + 1)))
+        (line_no + 1)))
 
 
-def exceptionWrongSpace():
-    exit("Analisador Sintatico -> Linha : {}, código após finalização do programa.".format((Lexico.n_line + 1)))
+def exceptionWrongSpace(line_no):
+    exit("Analisador Sintatico -> Linha : {}, código após finalização do programa.".format((line_no + 1)))
 
 
-def exceptionMissingDot():
-    exit("Analisador Sintatico -> Linha : {}, token '.' esperado.".format((Lexico.n_line + 1)))
+def exceptionMissingDot(line_no):
+    exit("Analisador Sintatico -> Linha : {}, token '.' esperado.".format((line_no + 1)))
 
 
-def exceptionMissingStart():
-    exit("Analisador Sintatico -> Linha : {}, missing start.".format((Lexico.n_line + 1)))
+def exceptionMissingStart(line_no):
+    exit("Analisador Sintatico -> Linha : {}, missing start.".format((line_no + 1)))
