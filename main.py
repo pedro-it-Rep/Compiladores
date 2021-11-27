adea889be416e5b43ff2ec20a6e83bb5295ac5e7
Fabrício <53907830+Fabricio-Silva-Cardoso1@users.noreply.github.com>
2021-11-27 20:26:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #from Sintatico import Sintatico
 #from MaquinaVirtual.VM import VM
 from MaquinaVirtual.VMUI import VMUI
+#from Tela import Tela
 
 if __name__ == '__main__':
     #Sintatico.Sintatico(Sintatico)
     VMUI.printVMUI(VMUI)
+    #Tela.mainTela(Tela)
